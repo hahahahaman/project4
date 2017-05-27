37878d6275150fcc1a7776e4a0a22e762d84d945
Yuhang Zhang <yuhang174@users.noreply.github.com>
2017-05-27 19:54:45 -0400

    clean up environment

--- Assets/_Scripts/CameraControl.cs
+++ Assets/_Scripts/CameraControl.cs
@@ -5,16 +5,42 @@ public class CameraControl : MonoBehaviour {
 
 	public GameObject player;
 
-	private Vector3 offset;
+	//Vars for camera rotation speed.
+	public float rotationSpeed = 70;
 
-	// Use this for initialization
-	void Start () {
-		offset = transform.position - player.transform.position;
-	}
+	// Vars for camera transform.
+	private Vector3 offset = new Vector3(0, 2.5f, -3.5f);
+	private Quaternion rotationOffset = Quaternion.identity;
 
-	// Update is called once per frame
-	void LateUpdate () {
+	void InitiateCameraTransform() {
+		rotationOffset.eulerAngles = new Vector3(25, 0, 0);
 		transform.position = player.transform.position + offset;
+		transform.rotation = rotationOffset;
 	}
+
+
+
+	void RotateCamera() {
+		if (Input.GetAxis ("Mouse X") > 0) {
+			transform.RotateAround (player.transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
+		} else if (Input.GetAxis ("Mouse X") < 0) {
+			transform.RotateAround (player.transform.position, Vector3.down, rotationSpeed * Time.deltaTime);
+		} else if (Input.GetAxis ("Mouse Y") > 0) {
+			transform.RotateAround (player.transform.position, Vector3.right, rotationSpeed * Time.deltaTime);
+		} else if (Input.GetAxis ("Mouse Y") < 0) {
+			transform.RotateAround (player.transform.position, Vector3.left, rotationSpeed * Time.deltaTime);
+		}	
+	}
+
+	void Start () {
+		InitiateCameraTransform ();
+	}
+		
+	void LateUpdate () {
+		RotateCamera ();
+		//transform.position = player.transform.position + offset;
+		//
+	}
+		
 }
 
---
+++ Assets/_Scripts/StartUpScripts.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartUpScripts : MonoBehaviour {
+
+	void Start () {
+		Cursor.visible = false;
+	}
+}
